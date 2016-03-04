331ebd57a3e77d34aed21e8d5576ab8310203ced
Sashtastic <ht.sasha.tran@gmail.om>
2016-03-04 02:05:49 -0500

    jiohohohi

--- js/main.js
+++ js/main.js
@@ -1,54 +1,9 @@
-//alert('Hello');
-//alert('Really?');
-//document.write('Here');
+var aLetter = prompt('Enter a letter, darling:');
 
-var = "Luke"
-var my_Name = 'NoticeMe'
-var myLastName = 'Senpai'
-alert(my_Name + ' ' + myLastName);
+for (a = 0; a < 10; a++) {
+  document.write(aLetter);
+  document.write('<br>');
+  document.write(aLetter+=aLetter);
+  document.write('<br>');
 
-var age = 666;
-var numbra = 999;
-var penumbra = 13;
-
-//alert(age + penumbra);
-//alert(age * penumbra);
-//alert(age / penumbra);
-//alert(age - penumbra);
-
-var newNum = numbra - penumbra;
-
-alert(newNum);
-
-var isNoticed = true;
-var isNotNoticed = false;
-
-if (isNoticed &&! isNotNoticed) {
-	alert("Senpai noticed me!");
-} else {
-	alert("Notice me, Senpai");
-}
-
-if (isNoticed || isNotNoticed) {
-	alert("Senpai just noticed me again!");
-} else {
-	alert("Y U NO NOTICE ME, SENPAI?!");
-}
-
-if (age > 5) {
-	alet ('Over 5');
-} else {
-	alert ('Under 5');
-}
-
-if (age >= 25 && isNoticed && isNotNoticed) {
-	alert ('Senpai finally notice me');	
-}
-
-var newName = prompt('What is your name?')
-
-if (newName == 'Luke') {
-	alert ('Luke, I am your father');
-} else {
-	alert ('Feel the force');
 }
